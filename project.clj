4fb6bfa111dc927a8fa4d2d5e4140e82f264c176
Gunnar Völkel <Gunnar.Voelkel@uni-ulm.de>
2012-07-14 18:34:47 +0200

    Update auf SingX 1.6.3.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject clj-gui "0.2.0"
+(defproject clj-gui "0.2.1"
 	:dependencies 
 	[[org.clojure/clojure 			"1.2.1"]
    [org.clojure/core.incubator "0.1.0"]
-   [org.swinglabs/swingx-core		"1.6.2-2"]]
+  ;[org.swinglabs/swingx-core		"1.6.2-2"]]
+   [org.swinglabs.swingx/swingx-core "1.6.3"]]
 
 	:source-path "src")
