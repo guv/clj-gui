5b7ebee41c9fe7801490898a3613aeaa21cce40a
Gunnar Völkel <Gunnar.Voelkel@uni-ulm.de>
2013-07-16 14:56:24 +0200

    Adds license and description.

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject clj-gui "0.3.1"
+  :description "GUI library for SwingX JXTreeTable and tools."
+  :url "https://github.com/guv/clj-debug"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
 	:dependencies 
 	[[org.clojure/clojure 			"1.5.1"]
